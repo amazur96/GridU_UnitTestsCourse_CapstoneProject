bdc01f65e0e4aad0aaf4ce355daafed1e27b9ee0
Artur Mazur <amazur@griddynamics.com>
2022-03-18 14:47:02 +0200

    add: Users fragment and users viewmodel tests

---
+++ app/src/androidTest/java/com/example/gridu_unittestscourse_capstoneproject/ui/users/UsersFragmentTest.kt
@@ -0,0 +1,61 @@
+package com.example.gridu_unittestscourse_capstoneproject.ui.users
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.filters.MediumTest
+import com.example.gridu_unittestscourse_capstoneproject.R
+import com.example.gridu_unittestscourse_capstoneproject.data.model.UserDetails
+import com.example.gridu_unittestscourse_capstoneproject.data.source.FakeUserRepository
+import com.example.gridu_unittestscourse_capstoneproject.di.launchFragmentInHiltContainer
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.Matchers.not
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import javax.inject.Inject
+
+@MediumTest
+@HiltAndroidTest
+@ExperimentalCoroutinesApi
+class UsersFragmentTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var repository: FakeUserRepository
+
+    private val users = listOf(
+        UserDetails(0), UserDetails(1), UserDetails(2),
+        UserDetails(3), UserDetails(4)
+    )
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun usersScreen_displayedFullDataInUi() {
+        runBlocking {
+            users.forEach { repository.saveUser(it) }
+        }
+        launchFragmentInHiltContainer<UsersFragment>()
+
+        onView(withId(R.id.usersRecyclerView)).check(matches(isDisplayed()))
+        onView(withId(R.id.emptyMessageFrameLayout)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.errorMessageFrameLayout)).check(matches(not(isDisplayed())))
+    }
+
+    @Test
+    fun usersScreen_displayedEmptyDataInUi() {
+        launchFragmentInHiltContainer<UsersFragment>()
+
+        onView(withId(R.id.emptyMessageFrameLayout)).check(matches(isDisplayed()))
+    }
+}
